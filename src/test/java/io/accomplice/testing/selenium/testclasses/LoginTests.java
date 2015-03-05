48e2edfdd120cbde8feec4b162f5ea33451d1546
MABH <manujbhalla@outlook.com>
2015-03-05 19:22:49 +0530

    login testing

--- src/test/java/io/accomplice/testing/selenium/testclasses/LoginTests.java
+++ src/test/java/io/accomplice/testing/selenium/testclasses/LoginTests.java
@@ -10,22 +10,39 @@
 
 package io.accomplice.testing.selenium.testclasses;
 
+import io.accomplice.testing.selenium.utils.LoginUtils;
+
+import java.util.Map;
+
 import junit.framework.Assert;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.TestException;
 import org.testng.annotations.Test;
 
+import com.google.common.collect.ImmutableMap;
+
 @Test(groups = {"LOGIN-1"})
-public class LoginTests {
+public final class LoginTests {
 
 	@Test
-	public void test_Login1() {
-		System.out.println("runnig test1");
-		Assert.assertTrue(true);
+	public void testLogin() {
+		final WebDriver driver = new FirefoxDriver();
+		
+		Map<String, Object> args = ImmutableMap.<String, Object>builder()
+										.put("DRIVER", driver)
+										.put("URL", "http://alpha.accomplice.io")
+										.put("USERNAME", "deepika.arora@accomplice.io")
+										.put("PASSWORD", "deepika")
+										.build();
+										
+		try {
+			LoginUtils.login(args);
+			Assert.assertTrue(true);
+		} catch(TestException e) {
+			Assert.assertTrue(e.getMessage(), false);
+		}
 	}
-
-	@Test
-	public void test_Login2() {
-		System.out.println("runnig test2");
-		Assert.assertTrue(true);
-	}
+	
 }
